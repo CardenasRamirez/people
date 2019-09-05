ef26ee584c76d815d95531e637ffd48ee3e23195
CDS2UNIVO19 <CDS2UNIVO19@CDS2UNIVO19>
2019-09-05 08:47:29 -0600

    Class model person added

---
+++ people/people/Models/person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace people.Models
+{
+    [Table("Tbl_People")]
+    class person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id{ get; set;}
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+   
+}
