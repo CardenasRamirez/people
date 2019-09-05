cb12b06f9d7d536d2aba17f84da77caf2a18f399
CDS2UNIVO19 <CDS2UNIVO19@CDS2UNIVO19>
2019-09-05 09:10:02 -0600

    tabla person creada

---
+++ people/people/PersonRespositoryCRUD.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using people.Models;
+
+namespace people
+{
+   public class PersonRespositoryCRUD
+    {
+        SQLiteConnection conn;
+        public string StatusMessage { get; set; }
+
+        //Constructor de la conexion
+        public PersonRespositoryCRUD(string dbPath)
+        {
+            //Creamos la coneccion
+            conn = new SQLiteConnection(dbPath);
+            conn.CreateTable<person>();
+        }
+    }
+}
